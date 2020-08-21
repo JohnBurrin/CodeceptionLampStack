931d17178bb7e1b2dbffd23d3fc676d93383851e
John <john@synergitech.co.uk>
2020-08-21 13:18:14 +0100

    Switch to localhost

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -14,11 +14,6 @@ class FirstCest
     public function frontpageWorks(AcceptanceTester $I)
     {
         $I->amOnPage('/');
-//        $I->see('LAMP STACK');
-        $I->seeInTitle('Google');
-        $I->fillField('q', 'php');
-        $I->click('btnK');
-        $I->seeInTitle('php - Google Search');
-        $I->see("PHP: Hypertext Preprocessor");
+        $I->see('LAMP STACK');
     }
 }
