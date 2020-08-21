0e9bc2036d94f8f655366d174f49aa121172e51d
John <john@synergitech.co.uk>
2020-08-21 13:35:34 +0100

    Simple Tests

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -16,4 +16,25 @@ class FirstCest
         $I->amOnPage('/');
         $I->see('LAMP STACK');
     }
+
+    public function clickPhpInfoLink(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('phpinfo()');
+        $I->seeInTitle('PHP 7.4.2 - phpinfo()');
+    }
+
+    public function testDBConnectionMysqli(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('Test DB Connection with mysqli');
+        $I->see('Success:');
+    }
+
+    public function testDBConnectionPdo(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('Test DB Connection with PDO');
+        $I->see('Looking good');
+    }
 }
